6e0864deefce3f95b19dceaa35110852b95947f2
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2019-11-23 15:58:19 -0300

    practice

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// app.use((req, res, next) => {
+//     console.log('first');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('second');
+//     next();
+// });
+
+app.use('/users', (req, res) => {
+    console.log('/users');
+    res.send('Navigated to /users');
+});
+
 app.use('/', (req, res) => {
-    console.log('navigated to /');
-    res.send('Ok');
+    console.log('/');
+    res.send('Navigated to /');
 });
 
 app.listen(3000, () => {
