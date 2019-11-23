ae6e905b2182417f85f7547b0e3185f15bca8bd3
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2019-11-23 15:43:31 -0300

    express #1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('working');
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+    console.log('navigated to /');
+    res.send('Ok');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
