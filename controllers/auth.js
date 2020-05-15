56d054917226fd84e2ee6c16d370304687ccedfc
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-05-14 23:36:42 -0300

    15_advanced-authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
+const crypto = require('crypto');
+
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
-const { BCRYPT_SALT, SENDGRID_API_KEY } = process.env;
+const { BCRYPT_SALT, SENDGRID_API_KEY, MAIL_SENDER } = process.env;
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -95,11 +97,89 @@ exports.postSignup = (req, res) => {
           res.redirect('/login');
           return transporter.sendMail({
             to: email,
-            from: 'xesque@nodejsstudy.com',
+            from: MAIL_SENDER,
             subject: 'Signup complete',
             html: '<h1>Xesquedele!</h1>'
           });
         });
     })
+    .catch(err => console.log(err));
+};
+
+exports.getResetPassword = (req, res) => {
+  res.render('auth/reset', {
+    routePath: '/reset',
+    pageTitle: 'Reset password'
+  });
+};
+
+exports.postResetPassword = (req, res) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+
+    const token = buffer.toString('hex');
+    const { email } = req.body;
+
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'E-mail not found.');
+          return res.redirect('/reset');
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 360000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        return transporter.sendMail({
+          to: email,
+          from: MAIL_SENDER,
+          subject: 'Password reset',
+          html: `
+            <p>Click the link below to set a new password</p>
+            <p><a href="http://localhost:3000/update-password/${token}">Xesque</a></p>
+          `
+        })
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+exports.getUpdatePassword = (req, res) => {
+  const { token } = req.params;
+
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      res.render('auth/update-password', {
+        routePath: '/update-password',
+        pageTitle: 'Update password',
+        userId: user._id.toString(),
+        resetToken: token
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postUpdatePassword = (req, res) => {
+  const { userId, password, resetToken } = req.body;
+  let resetUser;
+  
+  User.findOne({ _id: userId, resetToken, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(password, parseInt(BCRYPT_SALT, 10))
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(() => res.redirect('/login'))
     .catch(err => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,9 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+router.get('/reset', authController.getResetPassword);
+router.post('/reset', authController.postResetPassword);
+router.get('/update-password/:token', authController.getUpdatePassword);
+router.post('/update-password', authController.postUpdatePassword);
 
 module.exports = router;
